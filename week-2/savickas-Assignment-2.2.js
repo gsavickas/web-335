d2aae73dd06e44d8c040d57434f80d568abaeda8
Grayton Savickas <gsavickaspro@gmail.com>
2021-03-28 20:33:22 -0500

    adds assignment 2.2

---
+++ week-2/savickas-Assignment-2.2.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  Personal Portfolio
+; Author: Grayton Savickas
+; Date:   28 March 2021
+; Modified By:
+; Description: UML relational data structure NoSQL
+;===========================================
+*/
+[
+    {
+        "id": "00001",
+        "firstName": "Darth",
+        "lastName": "Vader",
+        "invoice":[
+            {
+                "id": "12345",
+                "date_created": "4 May 2021",
+                "date_shipped": "5 May 2021",
+                "subtotal": "65.00",
+                "tax": "1.66",
+                "total": "66.66",
+                "lineItem":[
+                    {
+                        "id": "12345-1",
+                        "name": "Death Star",
+                        "price": "1,370,000,000,000,000,000,000",
+                        "quantity": "2"
+                    }
+                ]
+            }
+    ],
+    }
+]
