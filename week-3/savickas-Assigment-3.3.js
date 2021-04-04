c51f16887a13d0d19e28a8c3ef54b4abfc76293f
Grayton Savickas <gsavickaspro@gmail.com>
2021-04-04 15:56:31 -0500

    adds assignment 3.3

---
+++ week-3/savickas-Assigment-3.3.js
@@ -0,0 +1,31 @@
+
+[
+    {
+            "book_name":"Slaughterhouse-Five",
+            "book_price": "25.00",
+            "book_isbn": "0-385-31208-3",
+            "author": [
+                {
+                    "author_first_name": "Kurt",
+                    "author_last_name": "Vonnegut",
+                    "author_phone": "555-555-5555",
+                    "author_email": "kvonnegut@gmail.com"
+                }
+            ],
+            "publisher":[
+                {
+                    "publisher_name": "Dell",
+                    "publisher_code": "33556",
+                    "publisher_address": [
+                       {
+                          "street": "800 East Ninety-sixth Street",
+                          "city": "Indianapolis",
+                          "state": "IN",
+                          "zip": "46240",
+                          "country": "United States"
+                       } 
+                    ]
+                }
+            ]
+    }
+]
