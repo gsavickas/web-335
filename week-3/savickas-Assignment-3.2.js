c8c7b41695617c69128394cbf19bceea682e9c72
Grayton Savickas <gsavickaspro@gmail.com>
2021-04-04 13:05:15 -0500

    adds assignment 3.2

---
+++ week-3/savickas-Assignment-3.2.js
@@ -0,0 +1,29 @@
+[
+    {
+        "user_id": "12345",
+        "first_name": "12345",
+        "last_name": "12345",
+        "user_roles": [
+            {
+                "base_user": [
+                    {
+                        "enabled": "true",
+                        "read_permission": "true",
+                        "write_permission": "true"
+                    }
+                ],
+                "admin": [
+                    {
+                        "edit_content":"false",
+                        "super_user": "false"
+                    }
+                ],
+                "QA":[
+                    {
+                        "edit_comments":"false"
+                    }
+                ]
+            }
+        ]
+    }
+]
