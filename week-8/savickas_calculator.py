68780983b64e12bc30d797962a395c0a6c53d14a
Grayton Savickas <graytonsavickas@Mandalorian.local>
2021-05-06 21:04:12 -0500

    adds assignment 8.3

---
+++ week-8/savickas_calculator.py
@@ -0,0 +1,27 @@
+# /*
+# ============================================
+# ; Title:  calculator
+# ; Author: Professor Krasso
+# ; Date:   6 May 2021
+# ; Modified By: Grayton Savickas
+# ; Description: Performs basic calcuator operations with functions in python
+# ;===========================================
+# */
+
+# This is the addition function. It takes 2 numbers and adds them. It then returns the output.
+def add(item1, item2):
+  return(item1 + item2)
+
+# This is the subtraction function. It takes 2 numbers and subtracts them. It then returns the output.
+def subtract(item1, item2):
+  return(item1 - item2)
+
+# This is the divide function. It takes 2 numbers and divides the first by the second. It then returns the output.
+def divide(item1, item2):
+  return(item1 / item2)
+
+print(add(5,5))
+
+print(subtract(6,3))
+
+print(divide(6,3))
